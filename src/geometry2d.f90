9a72f8f8596169379480cacd1396a8094a7b9903
Matt Blomquist <blomquist.matt@gmail.com>
2018-04-03 10:20:26 -0700

    Update geometry2d.f90

--- src/geometry2d.f90
+++ src/geometry2d.f90
@@ -1,7 +1,7 @@
 ! geometry2d Subroutine for 2D CFD Problems
 !
 ! Written by Matt Blomquist
-! Last Update: 2018-02-19 (YYYY-MM-DD)
+! Last Update: 2018-04-03 (YYYY-MM-DD)
 !
 ! This subroutine calculates the geometry properties and associated
 ! non-dimensional quantities for use in the SIMPLER method.
@@ -14,11 +14,7 @@ subroutine geometry2d
   ! Calculate non-dimensional quantities for SIMPLER
   dx = length/m
   dy = width/n
-  dz = depth/l
-
-  A_x = dy*dz
-  A_y = dz*dx
-  A_z = dx*dy
+  dz = 1
 
   return
 
