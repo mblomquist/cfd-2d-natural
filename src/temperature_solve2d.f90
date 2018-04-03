bc0d760f77f7c6a144eaa3b4e96603928dbd68f3
Matt Blomquist <blomquist.matt@gmail.com>
2018-04-03 10:23:40 -0700

    Update temperature_solve2d.f90

--- src/temperature_solve2d.f90
+++ src/temperature_solve2d.f90
@@ -1,7 +1,7 @@
 ! temperature_solve2d Subroutine for 2D CFD Problems
 !
 ! Written by Matt Blomquist
-! Last Update: 2018-03-09 (YYYY-MM-DD)
+! Last Update: 2018-04-03 (YYYY-MM-DD)
 !
 ! This subroutine solves the temperature equation of the SIMPLER algorithm
 !
@@ -14,7 +14,7 @@ subroutine temperature_solve2d
   call temperature_source2d
 
   ! Solve velocity Equations
-  call solver2d_bicgstab(As_T, Aw_T, Ap_T, Ae_T, An_T, b_T, T, m-1, n-1, tol, maxit)
+  call solver2d_bicgstab(As_T, Aw_T, Ap_T, Ae_T, An_T, b_T, T, m-1, n-1, solver_tol, maxit)
 
   return
 
