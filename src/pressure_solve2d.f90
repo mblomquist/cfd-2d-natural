a8dd534fce6bb22547280e7134b56669bbac88ff
Matt Blomquist <blomquist.matt@gmail.com>
2018-04-03 10:22:15 -0700

    Update pressure_solve2d.f90

--- src/pressure_solve2d.f90
+++ src/pressure_solve2d.f90
@@ -1,7 +1,7 @@
 ! pressure_solve2d Subroutine for 2D CFD Problems
 !
 ! Written by Matt Blomquist
-! Last Update: 2018-03-08 (YYYY-MM-DD)
+! Last Update: 2018-04-03 (YYYY-MM-DD)
 !
 ! This subroutine solves the pressure equation for the SIMPLER algorithm
 ! using u_hat and v_hat.
@@ -16,74 +16,56 @@ subroutine pressure_solve2d
   ! Define internal variables
   integer :: i, j
 
-  ! Update reference point :: set to 0
-  Ae_p(m-1,1) = 0
-  Aw_p(m-1,1) = 0
-  An_p(m-1,1) = 0
-  As_p(m-1,1) = 0
-
-  Ap_p(m-1,1) = 1
-  b_p(m-1,1) = 0
-
-  ! Update east boundary :: outlet boundary // no link to Ae_p
-  ! Set i
-  i = m-1
-
-  do j = 2,n-1
-
-    ! Update coefficients
-    Ae_p(i,j) = 0
-    Aw_p(i,j) = 0 !rho*A_x/Ap_u(i,j)*alpha
-    An_p(i,j) = 0 !rho*A_y/Ap_v(i,j+1)*alpha
-    As_p(i,j) = 0 !rho*A_y/Ap_v(i,j)*alpha
-
-    if (j .eq. 1) then
-      As_p(i,j) = 0
-    end if
-
-    if (j .eq. n-1) then
-      An_p(i,j) = 0
-    end if
-
-    Ap_p(i,j) = 1 !As_p(i,j)+Aw_p(i,j)+Ae_p(i,j)+An_p(i,j)-Sp_p(i,j)
-
-    ! Update b values
-    b_p(i,j) = 0 !rho*(v_hat(i,j)-v_hat(i,j+1))*A_y+Su_p(i,j)
-
-  end do
-
-  ! Update internal coefficients
-  do i = 1,m-2
+  ! Update coefficients
+  do i = 1,m-1
     do j = 1,n-1
 
       ! Update coefficients
-	    Ae_p(i,j) = rho*A_x/Ap_u(i+1,j)
-	    Aw_p(i,j) = rho*A_x/Ap_u(i,j)
-	    An_p(i,j) = rho*A_y/Ap_v(i,j+1)
-	    As_p(i,j) = rho*A_y/Ap_v(i,j)
+	  Ae_p(i,j) = rho*dy/Ap_u(i+1,j)
+	  Aw_p(i,j) = rho*dy/Ap_u(i,j)
+	  An_p(i,j) = rho*dx/Ap_v(i,j+1)
+	  As_p(i,j) = rho*dx/Ap_v(i,j)
 
+	  ! Check west node
       if (i .eq. 1) then
         Aw_p(i,j) = 0
       end if
 
+	  ! Check east node
+	  if (i .eq. m-1) then
+	    Ae_p(i,j) = 0
+	  end if
+
+	  ! Check south node
       if (j .eq. 1) then
         As_p(i,j) = 0
       end if
 
+	  ! Check north node
       if (j .eq. n-1) then
         An_p(i,j) = 0
       end if
 
+	  ! Ap coefficient
       Ap_p(i,j) = As_p(i,j)+Aw_p(i,j)+Ae_p(i,j)+An_p(i,j)-Sp_p(i,j)
 
-	    ! Update b values
-	    b_p(i,j) = rho*(u_hat(i,j)-u_hat(i+1,j))*A_x+rho*(v_hat(i,j)-v_hat(i,j+1))*A_y+Su_p(i,j)
+	  ! Update b values
+	  b_p(i,j) = rho*(u_hat(i,j)-u_hat(i+1,j))*dy+rho*(v_hat(i,j)-v_hat(i,j+1))*dx+Su_p(i,j)
 
     end do
   end do
 
+  ! Set reference pressure node (east-north corner)
+  Aw_p(m-1,n-1) = 0
+  Ae_p(m-1,n-1) = 0
+  As_p(m-1,n-1) = 0
+  An_p(m-1,n-1) = 0
+
+  Ap_p(m-1,n-1) = 1
+  b_p(m-1,n-1) = 0
+
   ! Solve pressure equation
-  call solver2d_bicgstab(As_p, Aw_p, Ap_p, Ae_p, An_p, b_p, P, m-1, n-1, tol, maxit)
+  call solver2d_bicgstab(As_p, Aw_p, Ap_p, Ae_p, An_p, b_p, P, m-1, n-1, solver_tol, maxit)
 
   return
 
