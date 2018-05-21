179eb70b0eeac7d95f38c6e1eef6a2ea1e52c74a
Matt <blomquist.matt@gmail.com>
2018-05-21 10:59:09 -0700

    Update: don't update T.

--- src/simpler2d.f90
+++ src/simpler2d.f90
@@ -78,6 +78,8 @@ subroutine simpler2d
     print *, "v:", v
     print *, ".............."
 
+    return
+    
     ! Step 7: Solve Temperature Equation
     print *, "Step 6: Solve Temperature Equation"
     call temperature_solve2d
@@ -102,8 +104,6 @@ subroutine simpler2d
       print *, "Simpler completed in: ", i
       exit
     end if
-
-    return
 
   end do
 
