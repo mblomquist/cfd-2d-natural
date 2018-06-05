15a00fa0b8a64c9ab49f34d9240100981e02b153
Matt <blomquist.matt@gmail.com>
2018-06-05 15:18:18 -0700

    Update.

--- src/velocity_correct2d.f90
+++ src/velocity_correct2d.f90
@@ -20,7 +20,7 @@ subroutine velocity_correct2d
   ! Correct velocity values
   do i = 2,m-1
     do j = 1,n-1
-      u(i,j) = u_star(i,j)+length*dy/Ap_u(i,j)*(P_prime(i-1,j)-P_prime(i,j))*rho*u0**2.0
+      u(i,j) = u_star(i,j)+length**2.0*dy/Ap_u(i,j)*(P_prime(i-1,j)-P_prime(i,j))*rho*u0**2.0
     end do
   end do
 
@@ -44,7 +44,7 @@ subroutine velocity_correct2d
   ! Correct velocity values
   do i = 1,m-1
     do j = 2,n-1
-      v(i,j) = v_star(i,j)+length*dx/Ap_v(i,j)*(P_prime(i,j-1)-P_prime(i,j))*rho*u0**2.0
+      v(i,j) = v_star(i,j)+length**2.0*dx/Ap_v(i,j)*(P_prime(i,j-1)-P_prime(i,j))*rho*u0**2.0
     end do
   end do
 
