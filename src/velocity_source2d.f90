af0e4742051fee0a308a0c04124afb904caa9bd5
Matt <blomquist.matt@gmail.com>
2018-06-07 08:27:13 -0700

    Update.

--- src/velocity_source2d.f90
+++ src/velocity_source2d.f90
@@ -92,7 +92,7 @@ subroutine velocity_source2d(direction)
         end if
 
 		    ! Update b values
-		    b_u(i,j) = Su_u(i,j)*dx*dy+dy*(P_star(i-1,j)-P_star(i,j))
+		    b_u(i,j) = Su_u(i,j)*dx*dy+dy*(P_star(i-1,j)-P_star(i,j))*length
 
       end do
     end do
