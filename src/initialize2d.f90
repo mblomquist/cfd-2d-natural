b0e18ba1e2f1a63fe8436c2510eb4c4bc2926d2d
Matt <blomquist.matt@gmail.com>
2018-06-07 09:59:02 -0700

    Update.

--- src/initialize2d.f90
+++ src/initialize2d.f90
@@ -36,7 +36,7 @@ subroutine initialize2d
 
   ! Calculate Dimensionless Values
   Gr = Ra / Pr
-  Re = (Gr/10)**(0.5)
+  Re = (Gr/1)**(0.5)
 
   ! Calculate Scale Values
   length = ((Ra*nu**2)/(Pr*g*beta*delta_T))**(0.333)
