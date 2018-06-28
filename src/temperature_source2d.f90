c8ec236cce812e7339d7d0390bb51db5aaa227f1
Matt <blomquist.matt@gmail.com>
2018-06-28 14:08:01 -0700

    Update.

--- src/temperature_source2d.f90
+++ src/temperature_source2d.f90
@@ -22,10 +22,10 @@ subroutine temperature_source2d
     do j = 2,n-2
 
       ! Update convective terms
-      Fw = rho*dy*u(i,j)
-      Fe = rho*dy*u(i+1,j)
-      Fs = rho*dx*v(i,j)
-      Fn = rho*dx*v(i,j+1)
+      Fw = rho*dy*u(i,j)/2.
+      Fe = rho*dy*u(i+1,j)/2.
+      Fs = rho*dx*v(i,j)/2.
+      Fn = rho*dx*v(i,j+1)/2.
 
       ! Update diffusion terms
       Dw = dy/dx/Re/Pr
