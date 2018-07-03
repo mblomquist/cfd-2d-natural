658a9286ee02b3c87ac5247b7140e8fd036598fd
Matt <blomquist.matt@gmail.com>
2018-07-03 14:32:05 -0700

    Update: add k_const source.

--- src/temperature_source2d.f90
+++ src/temperature_source2d.f90
@@ -22,10 +22,10 @@ subroutine temperature_source2d
     do j = 2,n-2
 
       ! Update convective terms
-      Fw = dy*u(i,j)
-      Fe = dy*u(i+1,j)
-      Fs = dx*v(i,j)
-      Fn = dx*v(i,j+1)
+      Fw = k_const*dy*u(i,j)
+      Fe = k_const*dy*u(i+1,j)
+      Fs = k_const*dx*v(i,j)
+      Fn = k_const*dx*v(i,j+1)
 
       ! Update diffusion terms
       Dw = dy/dx/Pr/Ra**(0.5)
