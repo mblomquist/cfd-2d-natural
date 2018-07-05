87d09a0b77444a127b668bc69cba6e06d6a15239
Matt <blomquist.matt@gmail.com>
2018-07-05 09:51:37 -0700

    Update.

--- src/velocity2d.f90
+++ src/velocity2d.f90
@@ -26,12 +26,13 @@ subroutine velocity2d
 
   ! South boundary source terms :: none
   !Su_u(:, 1) = 0
-  Sp_u(:, 1) = -20*dx/dy/Ra**(0.5)
+
+  do j = 1,n-1
+    Sp_u(:, j) = -dx/Ra**(0.5)/(dy/2.0+(j-1.0)*dy)-dx/Ra**(0.5)/(dy/2.0+(n-1.0-j)*dy)
+  end do
 
   ! North boundary source terms :: none
   !Su_u(:, n-1) = 0
-  Sp_u(:, n-1) = -20*dx/dy/Ra**(0.5)
-
 
   ! ====================== V-Velocity ====================== !
   ! Initialize v-velocity field
@@ -45,11 +46,9 @@ subroutine velocity2d
 
   ! West boundary source terms :: no slip
   !Su_v(1, :) = 0
-  Sp_v(1, :) = -20*dx/dy/Ra**(0.5)
-
-  ! East boundary source terms :: no slip
-  !Su_v(m-1, :) = 0
-  Sp_v(m-1, :) = -20*dy/dx/Ra**(0.5)
+  do i = 1,m-1
+    Sp_v(i, :) = -dy/Ra**(0.5)/(dx/2.0+(i-1.0)*dx)-dy/Ra**(0.5)/(dx/2.0+(m-1.0-i)*dx)
+  end do
 
   return
 
